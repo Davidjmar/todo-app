d82060a60605123b2b41ce98ae7d055e654bdf01
David Martin <dj22martin@gmail.com>
2019-02-23 20:23:16 -0700

    Fourth Commit. All changes and development has occurred on the app.js and app.css files within src directory. Currently only the main todo-list container and header have been created and the next push should contain a fully formed header within the container that is an image with text 'To Do List' overlaying the background photo.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div className="todo-list-wrap">
+          Container
+          <div className="header-component">
+          Header
+          </div>
+        </div>
       </div>
     );
   }
